03aeea8806043da767e6c15340b6a13dbd17043d
jdalfaro4 <jdalfaro4@gmail.com>
2023-04-29 08:24:52 -0500

    update src-sw.js file and added to wepack.config to match jate

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -14,7 +14,7 @@ const initdb = async () =>
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
-console.log('Post to the database');
+console.log('Put to the database');
 
 // Create a connection to the database database and version we want to use.
 const jateDb = await openDB('jate', 1);
@@ -25,8 +25,11 @@ const tx = jateDb.transaction('jate', 'readwrite');
 // Open up the desired object store.
 const store = tx.objectStore('jate');
 
-// Use the .add() method on the store and pass in the content.
-const request = store.add(1, content);
+// Use the .put() method on the store and pass in the content.
+const request = store.put({
+  id: 1,
+  text: content
+});
 
 // Get confirmation of the request.
 const result = await request;
